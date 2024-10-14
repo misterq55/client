9ec92a8fbd1c3f197068b8211833fad08d489d07
규 서문 <illsi@naver.com>
2024-10-14 22:17:27 +0900

    register.tsx 작성중

--- pages/register.tsx
+++ pages/register.tsx
@@ -9,6 +9,15 @@ import React from "react";
                     <form>
                         
                     </form>
+                    <small>
+                        이미 가입하셨나요?
+                        <Link href="/login">
+                            <a>로그인</a>
+                        </Link>
+                    </small>
+                    <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border-gray-400'>
+                        회원 가입
+                    </button>
                 </div>
             </div>
         </div>
