d1bcb9641f2a11786bdd097ab7a2c9d81609b6d8
규 서문 <illsi@naver.com>
2024-10-17 22:10:37 +0900

    회원 가입 페이지

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from "react";
 
  const register = () => {
@@ -7,17 +8,16 @@ import React from "react";
                 <div className='w-10/12 mx-auto md:w-96'>
                     <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
                     <form>
-                        
+                        <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border-gray-400'>
+                            회원 가입
+                        </button>
                     </form>
                     <small>
                         이미 가입하셨나요?
-                        <Link href="/login">
-                            <a>로그인</a>
-                        </Link>
+                        {/* <Link href="/login">
+                            <a className='ml-1 text-blue-500 uppercase'>로그인</a>
+                        </Link> */}
                     </small>
-                    <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border-gray-400'>
-                        회원 가입
-                    </button>
                 </div>
             </div>
         </div>
