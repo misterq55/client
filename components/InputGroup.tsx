2f483afc0ef95d560d13301e353f29db2627e66e
규 서문 <illsi@naver.com>
2024-10-21 22:24:53 +0900

    섹션5 회원가입 로그인

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -23,7 +23,7 @@ const InputGroup: React.FC<InputGroupProps> = ({
             <input
                 type={type}
                 style={{minWidth: 300}}
-                className={cls(`w-full p-3 transition duration-200 boder boder-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+                className={cls(`w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
                     {"border-red-500": error}
                 )}
                 placeholder={placeholder}
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,22 +1,67 @@
+import InputGroup from '@/components/InputGroup';
+import axios from 'axios';
 import Link from 'next/link'
-import React from "react";
+import { useRouter } from 'next/router';
+import React, { FormEvent, useState } from "react";
 
- const register = () => {
+ const Register = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState<any>({});
+    
+    let router = useRouter();
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+
+        try {
+            const res = await axios.post('/auth/register', {
+                email,
+                password,
+                username
+            })
+            console.log('res', res);
+            router.push("/login");
+        } catch (error: any) {
+            console.log('error', error)
+            setErrors(error.response.data || {});
+        }
+    }
+
     return (
         <div className='bg-white'>
             <div className='flex flex-col items-center justify-content h-screen p-6'>
                 <div className='w-10/12 mx-auto md:w-96'>
                     <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
-                    <form>
+                    <form onSubmit={handleSubmit}>
+                        <InputGroup 
+                            placeholder='Email'
+                            value={email}
+                            setValue={setEmail}
+                            error={errors.email}
+                        />
+                        <InputGroup 
+                            placeholder='UserName'
+                            value={username}
+                            setValue={setUsername}
+                            error={errors.username}
+                        />
+                        <InputGroup 
+                            placeholder='Password'
+                            value={password}
+                            setValue={setPassword}
+                            error={errors.username}
+                        />
                         <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border-gray-400'>
                             회원 가입
                         </button>
                     </form>
                     <small>
                         이미 가입하셨나요?
-                        {/* <Link href="/login">
+                        <Link legacyBehavior href="/login">
                             <a className='ml-1 text-blue-500 uppercase'>로그인</a>
-                        </Link> */}
+                        </Link>
                     </small>
                 </div>
             </div>
@@ -24,4 +69,4 @@ import React from "react";
     )
  }
 
- export default register
+ export default Register
