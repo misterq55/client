2beb5e1aa943c3305e29223409fa6d5344db53db
규 서문 <illsi@naver.com>
2025-03-18 21:51:11 +0900

    커뮤니티 생성

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import InputGroup from '@/components/InputGroup';
+import { useAuthDispatch } from '@/context/auth';
 import axios from 'axios';
 import Link from 'next/link'
 import { useRouter } from 'next/router';
@@ -10,11 +11,17 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState<any>({});
 
+    const dispatch = useAuthDispatch()
+
     const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
 
         try {
-            const res = await axios.post('/auth/login', {password,username},{withCredentials: true} );
+            const res = await axios.post('/auth/login', {password,username}, {withCredentials: true} );
+
+            dispatch("LOGIN", res.data?.user);
+            router.push("/")
+
         } catch (error: any) {
             console.log('error', error)
             setErrors(error?.response?.data || {});
